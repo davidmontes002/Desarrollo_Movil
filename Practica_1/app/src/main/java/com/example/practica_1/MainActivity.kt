8387864d95028dceba6c1b62f0a6cad4a7402d27
davidmontes002 <VaddiTesmon0.2@outlook.com>
2026-09-10 09:39:00 -0700

    Primer commit

---
+++ Practica_1/app/src/main/java/com/example/practica_1/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.practica_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.practica_1.ui.theme.Practica_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Practica_1Theme{
+                AppNavigation()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Practica_1Theme {
+        Greeting("Android")
+    }
+}
